49284d2c5c012ce22504c538d9729fb83821a498
Nilesh2301 <nileshkrishesh@gmail.com>
2026-06-11 10:55:16 +0530

    C++

---
+++ Lecture1/SumOfNo.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int sum(int N){
+    int sum = 0;
+
+    for(int i=0; i<=N; i++){
+        sum += i;
+    }
+
+    return sum;
+}
+
+int main(){
+    cout << "Sum of 5 no. is " << sum(5) << endl;
+    return 0;
+}
