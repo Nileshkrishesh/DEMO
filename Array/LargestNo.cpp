7376ad5111683d1e3831c80792b95265d8aa6083
Nilesh2301 <nileshkrishesh@gmail.com>
2026-06-13 16:40:15 +0530

    min of number

--- Array/LargestNo.cpp
+++ Array/LargestNo.cpp
@@ -12,15 +12,15 @@ int main() {
         cin >> a[i];
     }
 
-    int max = a[0];
+    int min = a[0];
 
     for (int i = 1; i < n; i++) {
-        if (a[i] > max) {
-            max = a[i];
+        if (a[i] < min) {
+            min = a[i];
         }
     }
 
-    cout << max;
+    cout << min;
 
     return 0;
 }*/
