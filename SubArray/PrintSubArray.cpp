3978a39e56d5d69704c3a782721ee4e33e9e6a98
Nilesh2301 <nileshkrishesh@gmail.com>
2026-06-25 13:46:45 +0530

    SubArray

---
+++ SubArray/PrintSubArray.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter The size of array"<<endl;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    for(int st=0;st<n;st++){
+        for(int end=st;end<n;end++){
+            for(int i=st;i<=end;i++){
+                cout<<arr[i];
+            }
+        cout<<" ";
+        }
+        cout<<endl;
+
+    }
+}
