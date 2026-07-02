e41615c3bf8f0e97b5d08538ebc99a447fe78a19
Nilesh2301 <nileshkrishesh@gmail.com>
2026-07-02 13:43:26 +0530

    Selection Sort

--- Array/SelectionSort.cpp
+++ Array/SelectionSort.cpp
@@ -1,18 +1,17 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-void bubbleSort(vector<int>&arr,int n){//O(n^2)
+void selectionSort(vector<int>&arr,int n){//O(n^2)
     
     for(int i=0;i<n-1;i++){
-        bool swapped=false;//optimization
-        for(int j=0;j<n-i-1;j++){
-            if(arr[j]>arr[j+1]){
-                swap(arr[j],arr[j+1]);
+        int smallestIndex=i;
+        for(int j=i+1;j<n-i-1;j++){
+            
+            if(arr[j]<arr[smallestIndex]){
+                smallestIndex=j;
             }
         }
-        if(swapped==false){
-            break;
-        }
+        swap(arr[i],arr[smallestIndex]);
     }
 }
 void printArray(vector<int>&arr,int n){
@@ -24,7 +23,7 @@ void printArray(vector<int>&arr,int n){
 int main(){
     int n=5;
     vector<int> arr={4,1,5,2,3};
-    bubbleSort(arr,n);
+    selectionSort(arr,n);
     printArray(arr,n);
     return 0;
 }
