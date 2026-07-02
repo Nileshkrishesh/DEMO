5b94a39399dba3ca8c910e822e3c81424f5a3352
Nilesh2301 <nileshkrishesh@gmail.com>
2026-07-02 13:16:04 +0530

    Bubble sort

--- Array/BubbleSort.cpp
+++ Array/BubbleSort.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-void bubbleSort(vector<int>&arr,int n){
+void bubbleSort(vector<int>&arr,int n){//O(n^2)
     
     for(int i=0;i<n-1;i++){
+        bool swapped=false;//optimization
         for(int j=0;j<n-i-1;j++){
             if(arr[j]>arr[j+1]){
                 swap(arr[j],arr[j+1]);
             }
         }
+        if(swapped==false){
+            break;
+        }
     }
-    return 0;
 }
 void printArray(vector<int>&arr,int n){
     for(int i=0;i<n;i++){
